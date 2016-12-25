c868ae8f5bdef81e3c7fcb4cf2048110ac56fd77
Mirko Hecky <mirhec@nochains.de>
2016-12-25 15:26:36 +0100

    Started to add mongo db writer

--- src/management/insertBibles.js
+++ src/management/insertBibles.js
@@ -5,6 +5,9 @@ var models      = require('../models');
 var Parser      = require('xml2js').Parser;
 var Sequelize   = require('sequelize');
 var ProgressBar = require('progress');
+var assert      = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId    = require('mongodb').ObjectID;
 
 class BibleObject {
     constructor(bibleName, identifier, language) {
@@ -123,6 +126,142 @@ class SequelizeDbWriter {
     }
 }
 
+class MongoDbWriter {
+    constructor(bibleObject, progressBar) {
+        this.bar = progressBar;
+        if(!arguments.length)
+            this.bibleObj = new BibleObject();
+        else
+            this.bibleObj = bibleObject;
+    }
+    
+    writeInDb() {
+        var t = undefined;
+        var _this = this;
+        var url = 'mongodb://localhost:27017/strongs';
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server.");
+
+            // first create bible books
+            var cnt = fs.readFileSync('./bibles/bibleBooks_de.txt');
+            var books = (''+cnt).split('\n');
+            // var promises = [];
+            for (var i = 0, book; i < books.length; i++) {
+                book = books[i];
+                _this.findOrCreateBibleBook(i + 1, 'de', book, db);
+            }
+        });
+
+        return;
+
+
+
+        return models.sequelize.sync().then(() => {
+            // first create bible books
+            var cnt = fs.readFileSync('./bibles/bibleBooks_de.txt');
+            var books = (''+cnt).split('\n');
+            var promises = [];
+            for (var i = 0, book; i < books.length; i++) {
+                book = books[i];
+                promises.push(this.findOrCreateBibleBook(i + 1, 'de', book));
+            }
+            return Sequelize.Promise.all(promises).then(() => {
+                return this.findOrCreateBibleTranslation().then((trans) => {
+                    return Sequelize.Promise.map(this.bibleObj.verses, (vers) => {
+                        _this.bar.tick({'biblebook': vers.bookNumber});
+                        return models.BibleVers.findOne({transaction: t, where: {bookNr_id: vers.bookNumber, chapterNr: vers.chapterNumber, versNr: vers.versNumber}}).then((v) => {
+                            if (!(typeof v !== 'undefined' && v !== null)) {
+                                return models.BibleVers.create({bookNr_id: vers.bookNumber, chapterNr: vers.chapterNumber, versNr: vers.versNumber}, {transaction: t}).then((versObj) => {
+                                    return versObj;
+                                });
+                            }
+                            return v;
+                        }).then((v) => {
+                            return models.BibleSequence.findOne({transaction: t, where: {vers_id: v.id, translationIdentifier_id: trans.identifier}}).then((dbSeq) => {
+                                if (!(typeof dbSeq !== 'undefined' && dbSeq !== null)) {
+                                    return Sequelize.Promise.map(vers.sequences, (seq) => {
+                                        return models.BibleSequence.create({
+                                            vers_id: v.id, 
+                                            translationIdentifier_id: trans.identifier, 
+                                            sequenceText: seq.sequenceText, 
+                                            sequenceOrder: seq.sequenceIndex, 
+                                            strongNumber: seq.strongNumber,
+                                            grammar: seq.grammar},
+                                            {transaction: t});
+                                    });
+                                }
+                            });
+                        });
+                    }, {concurrency: 100});
+                });
+            });
+        });
+    }
+
+    findOrCreateBibleTranslation() {
+        return models.BibleTranslation.findOne({transaction: this.transaction, where: {identifier: this.bibleObj.identifier}}).then((t) => {
+            if (!(typeof t !== 'undefined' && t !== null)) {
+                t = models.BibleTranslation.create({identifier: this.bibleObj.identifier, language: this.bibleObj.language, name: this.bibleObj.title}, {transaction: this.transaction});
+            }
+            return t;
+        });
+    }
+
+    writeBibleBooks(trans) {
+        console.log('write bible books ...');
+        var cnt = fs.readFileSync('./bibles/bibleBooks_de.txt');
+        var iterable = (''+cnt).split('\n');
+        var promises = [];
+        for (var i = 0, book; i < iterable.length; i++) {
+            book = iterable[i];
+            promises.push(this.findOrCreateBibleBook(i + 1, 'de', book));
+        }
+        
+        return Sequelize.Promise.all(promises).then(function() {
+            promises = [];
+            var verses = this.bibleObj.verses;
+            for (var j = 0, vers; j < verses.length; j++) {
+                vers = verses[j];
+                promises.push(this.findOrCreateVers(trans, vers));
+            }
+            return Sequelize.Promise.all(promises);
+        });
+    }
+
+    findOrCreateBibleBook(nr, lang, sBook, db) {
+        var arr = sBook.split(',');
+
+        return db.collection('biblebook').findOne({nr: nr}).then((b) => {
+            if(b == null) {
+                console.log('couldnt find the bible book');
+                var alternatives = '';
+                if (arr.length > 2) {
+                    alternatives = ',' + arr.slice(2).join(',') + ',';
+                }
+                return db.collection('biblebook').insertOne({
+                    nr: nr, 
+                    name: arr[0], 
+                    short_name: arr[1], 
+                    alternativeNames: (typeof alternatives !== 'undefined' && alternatives !== null) ? alternatives : '', 
+                    language: lang
+                });
+            } else {
+                console.log('bible book found: ' + b);
+                return b;
+            }
+        });
+
+        // return models.BibleBook.findOne({transaction: this.transaction, where: {nr: nr}}).then((b) => {
+        //     if (!(typeof b !== 'undefined' && b !== null)) {
+        //         if (arr.length > 2) { var alternatives = ',' + arr.slice(2).join(',') + ','; }
+        //         b = models.BibleBook.create({nr: nr, name: arr[0], short_name: arr[1], alternativeNames: (typeof alternatives !== 'undefined' && alternatives !== null) ? alternatives : '', language: lang}, {transaction: this.transaction});
+        //     }
+        //     return b;
+        // });
+    }
+}
+
 
 var parseAndInsertBibles = function() {
     var file = './bibles/zefanja-xml/GER_ELB1905_STRONG.xml';
@@ -206,7 +345,8 @@ var parseAndInsertBibles = function() {
                 
                 // write in db
                 var bar = new ProgressBar(':percent, :elapsed s, current book: :biblebook :bar :current / :total verses', { total: bibleObj.verses.length });
-                var writer = new SequelizeDbWriter(bibleObj, bar);
+                // var writer = new SequelizeDbWriter(bibleObj, bar);
+                var writer = new MongoDbWriter(bibleObj, bar);
                 writer.writeInDb().then(function(t) {
                     console.log('done writing to database!');
                 });
